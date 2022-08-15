36f479adeaf827b4f2d7bff7ffa4785a81abec24
serAnkii <recruitankitnegi2023@gmail.com>
2022-08-15 21:35:38 +0530

    feat:some changes

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { RiPagesLine } from 'react-icons/ri'
+import { RiPagesLine  } from 'react-icons/ri'
+import {CgArrowLongRightC} from 'react-icons/cg'
 import TypewriterComponent from 'typewriter-effect'
 import "./about.css"
 export function About() {
@@ -14,14 +15,16 @@ export function About() {
     
     <>
     <div class="about">
-  <div class="grid-item1"> <h2>I'am a<br /></h2> <br /></div>
-  <div class="grid-item2"><p ><TypewriterComponent onInit={text} options={{autoStart:true,loop:true,}}/></p>
+<div id='typerwrite'>  
+  <div id='item1'> <h2>I'am a <CgArrowLongRightC/></h2></div>
+  <div >
+    <p id='item2' ><TypewriterComponent onInit={text} options={{autoStart:true,loop:true,}}/></p>
+  </div>
 </div>
-  <div class="grid-item3"><h1>This is my  <RiPagesLine/></h1></div>
-  <div class="grid-item4"><a href="https://drive.google.com/drive/folders/1cPsEL96CIrUwhvxv1ZcxV0Mo2zS6t_3m" target="_">RESUME </a>
-</div>
-  <div class="grid-item5">5</div>
-  <div class="grid-item6">6</div>
+  <div ><h1>This is my  <RiPagesLine/></h1></div>
+  <div >
+    <a href="https://drive.google.com/drive/folders/1cPsEL96CIrUwhvxv1ZcxV0Mo2zS6t_3m" target="_">RESUME </a>
+  </div>
 </div>
     
     </>
--- src/Components/Projectcards.jsx
+++ src/Components/Projectcards.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./cards.css"
 
 export default function Projectcards(props){
-
+    console.log(props.image)
     return (
         <>
         <div id="card" >
