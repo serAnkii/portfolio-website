37e49a4c14bfc872100b2144d5b82fc4f2fd1b3c
serAnkii <recruitankitnegi2023@gmail.com>
2023-04-13 09:23:33 +0530

    feat:redesigned the projectdetails page

--- src/App.jsx
+++ src/App.jsx
@@ -109,12 +109,12 @@ export default function App() {
           <Route path="/project" element={<Projects />} />
           <Route path="*" element={<Error />} />
 
-          <Route path="/projects/1" element={<Projectdescription name = {ele[0].name} description ={ele[0].description} context ={ele[0].context} role ={ele[0].role} skills ={ele[0].skills} outcome ={ele[0].outcome} visuals ={ele[0].visuals}  />} />
-          <Route path="/projects/2" element={<Projectdescription name = {ele[1].name} description ={ele[1].description} context ={ele[1].context} role ={ele[1].role} skills ={ele[1].skills} outcome ={ele[1].outcome} visuals ={ele[1].visuals} />} />
-          <Route path="/projects/3" element={<Projectdescription name = {ele[2].name} description ={ele[2].description} context ={ele[2].context} role ={ele[2].role} skills ={ele[2].skills} outcome ={ele[2].outcome} visuals ={ele[2].visuals} />} />
-          <Route path="/projects/4" element={<Projectdescription name = {ele[3].name} description ={ele[3].description} context ={ele[3].context} role ={ele[3].role} skills ={ele[3].skills} outcome ={ele[3].outcome} visuals ={ele[3].visuals} />} />
-          <Route path="/projects/5" element={<Projectdescription name = {ele[4].name} description ={ele[4].description} context ={ele[4].context} role ={ele[4].role} skills ={ele[4].skills} outcome ={ele[4].outcome} visuals ={ele[4].visuals} />} />
-          <Route path="/projects/6" element={<Projectdescription name = {ele[5].name} description ={ele[5].description} context ={ele[5].context} role ={ele[5].role} skills ={ele[5].skills} outcome ={ele[5].outcome} visuals ={ele[5].visuals} />} />
+          <Route path="/projects/1" element={<Projectdescription name = {ele[0].name} description ={ele[0].description} context ={ele[0].context} role ={ele[0].role} skills ={ele[0].skills} outcome ={ele[0].outcome} visuals ={ele[0].visuals} code ={ele[0].repo} deploy ={ele[0].deploy} />} />
+          <Route path="/projects/2" element={<Projectdescription name = {ele[1].name} description ={ele[1].description} context ={ele[1].context} role ={ele[1].role} skills ={ele[1].skills} outcome ={ele[1].outcome} visuals ={ele[1].visuals} code ={ele[1].repo} deploy ={ele[1].deploy}/>} />
+          <Route path="/projects/3" element={<Projectdescription name = {ele[2].name} description ={ele[2].description} context ={ele[2].context} role ={ele[2].role} skills ={ele[2].skills} outcome ={ele[2].outcome} visuals ={ele[2].visuals} code ={ele[2].repo} deploy ={ele[2].deploy}/>} />
+          <Route path="/projects/4" element={<Projectdescription name = {ele[3].name} description ={ele[3].description} context ={ele[3].context} role ={ele[3].role} skills ={ele[3].skills} outcome ={ele[3].outcome} visuals ={ele[3].visuals} code ={ele[3].repo} deploy ={ele[3].deploy}/>} />
+          <Route path="/projects/5" element={<Projectdescription name = {ele[4].name} description ={ele[4].description} context ={ele[4].context} role ={ele[4].role} skills ={ele[4].skills} outcome ={ele[4].outcome} visuals ={ele[4].visuals} code ={ele[4].repo} deploy ={ele[4].deploy}/>} />
+          <Route path="/projects/6" element={<Projectdescription name = {ele[5].name} description ={ele[5].description} context ={ele[5].context} role ={ele[5].role} skills ={ele[5].skills} outcome ={ele[5].outcome} visuals ={ele[5].visuals} code ={ele[5].repo} deploy ={ele[5].deploy}/>} />
         </Routes>
         <div className="wave">
           <svg
--- src/Components/Projectdescription.jsx
+++ src/Components/Projectdescription.jsx
@@ -1,25 +1,50 @@
 import React from "react";
 import "../css/projectdesc.css";
+
 const Projectdescription = (props) => {
   return (
-    <>
-      <div id="maincontainer">
-        <div id="nameofproject">
-          <h1>{props.name}</h1>
-        </div>
-
-        <div id="summary"> <p>{props.description}</p></div>
-
-        <div id="context">{props.context}</div>
-
-        <div id="role">{props.role}</div>
-        <div id="skills">{props.skills}</div>
-        <div id="Outcome">{props.outcome}</div>
-        <div id="Visuals">{props.visuals}</div>
-
-        <div id="codelinks"></div>
+    <div id="maincontainer">
+      <div id="nameofproject" className="pulse">
+        <h1>{props.name}</h1>
       </div>
-    </>
+
+      <div id="summary">
+        <p>{props.description}</p>
+      </div>
+
+      <div id="context">
+        <h3>Context</h3>
+        <br />
+        <p className="content">{props.context}</p>
+      </div>
+
+      <div id="role">
+        <h3>Role</h3>
+        <br />
+        <p className="content">{props.role}</p>
+      </div>
+      <div id="skills">
+        <h3>Skills</h3>
+        <br />
+        <p className="content">{props.skills}</p>
+      </div>
+      <div id="Outcome">
+        <h3>Outcome</h3>
+        <br />
+        <p className="content">{props.outcome}</p>
+      </div>
+      <div id="Visuals">
+        <h3>Visuals</h3> <p className="content">{props.visuals}</p>
+      </div>
+      <div class="code-links">
+        <a class="code-link" href={props.code}>
+          View Code
+        </a>
+        <a class="deploy-link" href={props.deploy}>
+          View Deployment
+        </a>
+      </div>
+    </div>
   );
 };
 
