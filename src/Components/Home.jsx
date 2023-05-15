81b0be3ab423621efb8ea6ba3220ac4cef5c780a
serAnkii <recruitankitnegi2023@gmail.com>
2023-05-15 08:40:51 +0530

    feat:changes to home component

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -4,39 +4,21 @@ import styled from "styled-components";
 import React from "react";
 
 const Home = () => {
-
   return (
-    <> 
+    <>
       <div id="home">
         <Container1>
-          <Text1 id="text1">
-            A WEB-DEVELOPER
-            {/* <span id="stars">
-              <h1>*</h1>
-              <h1>*</h1>
-              <h1>*</h1>
-              <h1>*</h1>
-            </span> */}
-          </Text1>
-          <Text2 id="text2">Your company can rely upon</Text2>
+          <Div id="div1">
+          <Text1 id="text1">Code</Text1> <Text2>is my paintbrush,</Text2>
+          </Div>
+          <Div id="div2">
+          <Text2>
+            and the</Text2> <Text1 id="text1">Web</Text1><Text2> is my canvas.</Text2>
+          </Div>
+          <Div id="div3">
+          <Text2 id="textdiv3">Let me craft an extraordinary online presence that leaves a lasting impression.</Text2>
+          </Div>
         </Container1>
-        {/* <Container1>
-          <Text1>A WEB-DEVELOPER</Text1>
-          <Text2>Your company can rely upon</Text2>
-        </Container1>
-        <Container1>
-          <Text1>A WEB-DEVELOPER</Text1>
-          <Text2>Your company can rely upon</Text2>
-        </Container1>
-        <Container1>
-          <Text1>A WEB-DEVELOPER</Text1>
-          <Text2>Your company can rely upon</Text2>
-        </Container1>
-        <Container1>
-          <Text1>A WEB-DEVELOPER</Text1>
-          <Text2>Your company can rely upon</Text2>
-        </Container1> */}
-        <div id="particles-js"></div>
       </div>
     </>
   );
@@ -46,27 +28,42 @@ export { Home };
 
 const Container1 = styled.div`
   position: relative;
-  flex-direction: column;
   height: 100vh;
   display: flex;
+  flex-direction:column;
   justify-content: center;
-  align-items: center;
   text-align: center;
+
 `;
 
+const Div = styled.div`
+  height:fit-content;
+  border-radius: 255px 15px 225px 15px/15px 225px 15px 255px;
+  border:dashed 1px white;
+  width:60vw;
+  display:flex;
+  align-items:center;
+  // justify-content:center;
+  gap:3rem;
+  padding:1rem;
+  letter-spacing:5px;
+`
+
 const Text1 = styled.h1`
-  font-size: 6rem;
+  font-size: 5rem;
   font-weight: bold;
   color: #2f2f2f;
-  transform: translate(-50%, -10%);
   z-index: 1;
   text-shadow: 0px 0px 10px rgba(0, 0, 0, 0.25);
+  height-fit-content;
+  margin:0;
 `;
 
 const Text2 = styled.h2`
-  font-size: 2.5rem;
+  font-size: 5rem;
   color: #4f4f4f;
-  transform: translate(-50%, -35%);
   z-index: 1;
-  color:white;
+  color: white;
+  font-family:"Roboto mono";
+  line-height:6rem;
 `;
