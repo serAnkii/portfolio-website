d97c30142db15f5448121562162869c6135b4c2c
serAnkii <recruitankitnegi2023@gmail.com>
2023-10-05 12:55:53 +0530

    feat:refactoring

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,55 +1,38 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../css/home.css";
-import styled from "styled-components";
-import { useRef } from "react";
+
 import { AiFillStar, AiOutlineStar } from "react-icons/ai";
 import { GiFallingStar } from "react-icons/gi";
 import { HiLightBulb, HiOutlineLightBulb } from "react-icons/hi";
 const Home = ({ isdark }) => {
-  const ref = useRef(null);
-  window.addEventListener("scroll", (e) => {});
+  
 
   return (
     <div className="common" id="header">
-      {/* ////lightmode/////// */}
-      {/* {isdark && <img src="/parallex/bg.png" alt="bg" className="lightparallex" />}
-      {isdark && <img src="/parallex/4.png" alt="" className="lightparallex" />}
-      {isdark && <img src="/parallex/3.png" alt="" className="lightparallex" />}
-      {isdark && <img src="/parallex/2nd.png" alt="" className="lightparallex" />}
-      {isdark && <img src="/parallex/front.png" alt="" className="lightparallex" />} */}
+      <div id="homediv">
+        <h1>Creating</h1>
 
-      {/* /////darkmode////// */}
-      {/* {!isdark && <div id="vignetee"></div>}
-      {!isdark && <img src="/moon.png" className="parallex" id="nightsky" />}
-      {!isdark && (
-        <h1 className="parallex" id="darkhomeheading">
-          Ohh! I see <br /> (ʘ ͜ʖ ʘ) !!!
-        </h1>
-      )}
-      {!isdark && (
-        <h2 id="darkhomepara" className="parallex">
-          You are searching for a potential Candidate.
-        </h2>
-      )}
-      {!isdark && (
-        <h1 className="parallex" id="darkhomepara2">
-          WELL
-        </h1>
-      )}
-      {!isdark && (
-        <h1 className="parallex" id="fourthpara">
-          You just Found The one
-        </h1>
-      )} */}
-
-      <h1>Creating Tommorow Together</h1>
+        <div className="waviy">
+          <span className="animetext" style={{ '--i': 2 }}>T</span>
+          <span className="animetext" style={{ '--i': 3 }}>O</span>
+          <span className="animetext" style={{ '--i': 4 }}>M</span>
+          <span className="animetext" style={{ '--i': 5 }}>M</span>
+          <span className="animetext" style={{ '--i': 6 }}>R</span>
+          <span className="animetext" style={{ '--i': 7 }}>O</span>
+          <span className="animetext" style={{ '--i': 8 }}>W</span>
+          
+    
+        </div>
+        <h1>Together</h1>
+      </div>
 
       <div id="starsdiv">
         <h2>Imagine The possiblites O_O</h2>
-        <i className="stars">{isdark?<HiLightBulb/>:<HiOutlineLightBulb/>}</i>
-        <i className="stars">{<GiFallingStar/>}</i>
-        <i className="stars">{isdark?<AiFillStar/>:<AiOutlineStar/>}</i>
-        
+        <i className="stars">
+          {isdark ? <HiLightBulb /> : <HiOutlineLightBulb />}
+        </i>
+        <i className="stars">{<GiFallingStar />}</i>
+        <i className="stars">{isdark ? <AiFillStar /> : <AiOutlineStar />}</i>
       </div>
     </div>
   );
