1616ddd9cd122bb6ae40db33c9eb46128a85698c
serAnkii <recruitankitnegi2023@gmail.com>
2023-10-06 15:04:46 +0530

    feat:refactoring

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,37 +1,41 @@
 import React, { useState } from "react";
 import "../css/about.css";
 import AboutNav from "./AboutNav";
+import {AiOutlineDownload} from "react-icons/ai"
 
 export function About() {
   return (
     <div className="common" id="about">
       <AboutNav />
 
-      <div id="greeting">
-        <h1>HELLO! </h1>
+      <div id="abouttext">
+        <div id="greeting">
+          <div className="greetingframe">
+          <h1>HELLO</h1>
+          </div>
+          <div id="seconditem" className="greetingframe">
+          <h1> I'm Ankit Negi </h1>
+          </div>
+        </div>
+
+        <div id="para">
+          <ul id="ulpara">
+            <li>I recently graduated in 2023 with a B.Tech in Computer Science from Graphic Era Hill University.</li>
+            <li>By day, I'm a Full Stack Web Developer equiped <br /> with the latest tools of the trade, crafting seamless digital experiences. Whether it's front-end or back-end, I've got it covered.</li>
+            <li>But wait, there's more! I'm not just about pixels and code; <br /> I'm also a Machine Learning enthusiast. I thrive on creating intelligent applications that tackle real-world challenges.</li>
+            <li> Ready to turn ideas into reality? <br /> Let's explore how I can contribute to your team!</li>
+          </ul>
+        </div>
+        <div id="resumediv">
+          <a href="/AnkitNegi.pdf" target="_blank" id="resume">
+            Resume <AiOutlineDownload/>
+          </a>
+        </div>
       </div>
 
-     
-
-      <div id="para">
-        <p>
-          I'm Ankit Negi, a recent B.tech(CSE) graduate actively seeking job
-          opportunities. My education and enthusiasm for technology drive
-          me to excel in the field.
-          Let's connect and explore how I can contribute to your team.
-
-        </p>
-        <a
-          href="/AnkitNegi.pdf"
-          target="_blank"
-          id="resume"
-        >Download <br /> Resume</a>
+      <div id="aboutimage">
+        <img src="/ankit.jpg" alt="" id="profile" />
       </div>
-
-      <img src="/Background.png" alt="" id="profile" />
-      
-       
-      
     </div>
   );
 }
