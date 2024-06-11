9a662c905d33b8e643f7f9129e0f2231ca2a363c
Ankit Negi <recruitankitnegi2023@gmail.com>
2024-06-11 05:07:27 +0000

    feat:some changes

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "../css/about.css";
 import AboutNav from "./AboutNav";
-import {AiOutlineDownload} from "react-icons/ai"
+import { AiOutlineDownload } from "react-icons/ai";
 
 export function About() {
   return (
@@ -11,30 +11,47 @@ export function About() {
       <div id="abouttext">
         <div id="greeting">
           <div className="greetingframe">
-          <h1>HELLO</h1>
+            <h1>HELLO</h1>
           </div>
           <div id="seconditem" className="greetingframe">
-          <h1> I'm Ankit Negi </h1>
+            <h1> I'm Ankit Negi </h1>
           </div>
         </div>
 
         <div id="para">
           <ul id="ulpara">
-            <li>I recently graduated in 2023 with a B.Tech in Computer Science from Graphic Era Hill University.</li>
-            <li>By day, I'm a Full Stack Web Developer equiped <br /> with the latest tools of the trade, crafting seamless digital experiences. Whether it's front-end or back-end, I've got it covered.</li>
-            <li>But wait, there's more! I'm not just about pixels and code; <br /> I'm also a Machine Learning enthusiast. I thrive on creating intelligent applications that tackle real-world challenges.</li>
-            <li> Ready to turn ideas into reality? <br /> Let's explore how I can contribute to your team!</li>
+            <li>
+              I recently graduated in 2023 with a B.Tech in Computer Science
+              from Graphic Era Hill University.
+            </li>
+            <li>
+              By day, I'm a Full Stack Web Developer equiped <br /> with the
+              latest tools of the trade, crafting seamless digital experiences.
+              Whether it's front-end or back-end, I've got it covered.
+            </li>
+            <li>
+              But wait, there's more! I'm not just about pixels and code; <br />{" "}
+              I'm also a Machine Learning enthusiast. I thrive on creating
+              intelligent applications that tackle real-world challenges.
+            </li>
+            <li>
+              {" "}
+              Ready to turn ideas into reality? <br /> Let's explore how I can
+              contribute to your team!
+            </li>
           </ul>
-        </div>
-        <div id="resumediv">
-          <a href="/Ankit_Negi.pdf" target="_blank" id="resume">
-            Resume <AiOutlineDownload/>
-          </a>
         </div>
       </div>
 
       <div id="aboutimage">
-        <img src="/ankit.jpg" alt="" id="profile" />
+        <div id="imagecontainer">
+        <img src="/ankit.png" alt="Profile Picture" id="profile" />
+        </div>
+       <div id="resumecontainer">
+       <a href="/Ankit_Negi.pdf" target="_blank" id="resume">
+            Resume <AiOutlineDownload/>
+        </a>
+       </div>
       </div>
     </div>
   );
